7605bf6a143da5ff0dd28f12c95435aa8ae92797
Emre Tepe <emretepedev@gmail.com>
2022-04-01 14:11:48 +0300

    initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  arrowParens: 'always',
+  bracketSameLine: false,
+  printWidth: 80,
+  useTabs: false,
+  quoteProps: 'as-needed',
+  overrides: [
+    {
+      files: '*.sol',
+      options: {
+        semi: true,
+        singleQuote: false,
+        tabWidth: 4,
+        trailingComma: 'none',
+        bracketSpacing: true,
+        arrowParens: 'avoid',
+        printWidth: 100,
+        useTabs: false,
+      },
+    },
+  ],
+};
---
+++ cspell.config.js
@@ -0,0 +1,49 @@
+module.exports = {
+  version: '0.2',
+
+  language: 'en',
+
+  words: [
+    // general
+    'jscoverage',
+    'hdwallet',
+    'solcjs',
+    'lcov',
+    'svmrc',
+    // projects
+    'nomiclabs',
+  ],
+
+  ignoreWords: [],
+
+  ignorePaths: ['.gitignore', 'yarn.lock', 'tags*', '.git', '.vscode'],
+
+  allowCompoundWords: true,
+
+  dictionaries: [
+    'javascript',
+    'typescript',
+    'npm',
+    'node',
+    'filetypes',
+    'softwareTerms',
+    'dockerfile',
+    'markdown',
+    'xml',
+    'yaml',
+    'bash',
+    'powershell',
+    'cpp',
+    'c',
+    'json',
+    'jsonc',
+    'plaintext',
+    'aws',
+    'public-licenses',
+    'cryptocurrencies',
+    'companies',
+    'misc',
+    'en-gb',
+    'lorem-ipsum',
+  ],
+};
