1ce79666743fadca3b5528d87865397eb64be9bb
Emir Güvenni <contact@emirguvenni.com>
2022-04-12 16:25:41 +0300

    Converted the Truffle test file to Hardhat test file

--- test/FooToken.test.ts
+++ test/FooToken.test.ts
@@ -1,64 +1,40 @@
-/* import { expect } from 'chai';
+import { config } from 'dotenv';
+config();
+
+import { expect, assert } from 'chai';
 import { ethers } from 'hardhat';
+import { BigNumber, Contract, Signer, constants } from 'ethers';
 
-describe('Greeter', function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory('Greeter');
-    const greeter = await Greeter.deploy('Hello, world!');
-    await greeter.deployed();
+if (!process.env.CONTRACT_1_NAME) throw Error('CONTRACT_1_NAME is not defined');
 
-    expect(await greeter.greet()).to.equal('Hello, world!');
+const contractName: string = process.env.CONTRACT_1_NAME;
 
-    const setGreetingTx = await greeter.setGreeting('world, Hello!');
+describe(contractName, () => {
+  const value: BigNumber = ethers.BigNumber.from('1');
+  let contract1: Contract;
+  let sender: Signer;
+  let receiver: Signer;
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
-
-    expect(await greeter.greet()).to.equal('world, Hello!');
-  });
-}); */
-
-require('dotenv').config();
-
-const {
-  BN, // Big Number support
-  constants, // Common constants, like the zero address and largest integers
-  expectEvent, // Assertions for emitted events
-  expectRevert, // Assertions for transactions that should fail
-} = require('@openzeppelin/test-helpers');
-
-const Contract_1 = await ethers.getContractFactory(process.env.CONTRACT_1_NAME);
-const _contract_1 = await Contract_1.deploy(
-  process.env.CONTRACT_1_TOKEN_SUPPLY
-);
-await _contract_1.deployed();
-
-contract(process.env.CONTRACT_1_NAME, ([sender, receiver]) => {
-  let contract_1;
-  const value = new BN(1);
-
-  // Hook that autoruns before each test function
-  beforeEach('should setup the contract instance', async () => {
-    // Set the contract
-    const contract_1 = await Contract_1.deploy(
-      process.env.CONTRACT_1_TOKEN_SUPPLY
-    );
-
-    await contract_1.deployed();
+  beforeEach(async () => {
+    [sender, receiver] = await ethers.getSigners();
+    const Contract1 = await ethers.getContractFactory(contractName);
+    contract1 = await Contract1.deploy(process.env.CONTRACT_1_TOKEN_SUPPLY);
+    await contract1.deployed();
   });
 
-  it('the token name should be correct', async () => {
-    const name = await contract_1.name();
-
-    assert.equal(
-      name,
-      process.env.CONTRACT_1_TOKEN_NAME,
-      'The token name must be valid.'
-    );
+  it('the token name should be correct', () => {
+    contract1.name().then((name: string) => {
+      assert.equal(
+        name,
+        process.env.CONTRACT_1_TOKEN_NAME,
+        'The token name must be valid.'
+      );
+      expect;
+    });
   });
 
   it('the token symbol should be correct', async () => {
-    const symbol = await contract_1.symbol();
+    const symbol = await contract1.symbol();
 
     assert.equal(
       symbol,
@@ -68,7 +44,7 @@ contract(process.env.CONTRACT_1_NAME, ([sender, receiver]) => {
   });
 
   it('the token decimal should be correct', async () => {
-    const decimals = (await contract_1.decimals()).toNumber();
+    const decimals = await contract1.decimals();
 
     assert.equal(
       decimals,
@@ -78,7 +54,7 @@ contract(process.env.CONTRACT_1_NAME, ([sender, receiver]) => {
   });
 
   it('the token supply should be correct', async () => {
-    const supply = (await contract_1.totalSupply()).toNumber();
+    const supply = (await contract1.totalSupply()).toNumber();
 
     assert.equal(
       supply,
@@ -88,25 +64,26 @@ contract(process.env.CONTRACT_1_NAME, ([sender, receiver]) => {
   });
 
   it('reverts when transferring tokens to the zero address', async () => {
+    const senderAddress = await sender.getAddress();
+
     // Conditions that trigger a require statement can be precisely tested
-    await expectRevert(
-      contract_1.transfer(constants.ZERO_ADDRESS, value, {
-        from: sender,
-      }),
-      'ERC20: transfer to the zero address'
-    );
+    await expect(
+      contract1.transfer(constants.AddressZero, value, {
+        from: senderAddress,
+      })
+    ).to.be.revertedWith('ERC20: transfer to the zero address');
   });
 
   it('emits a Transfer event on successful transfers', async () => {
-    const receipt = await contract_1.transfer(receiver, value, {
-      from: sender,
-    });
+    const senderAddress = await sender.getAddress();
+    const receiverAddress = await receiver.getAddress();
 
-    // Event assertions can verify that the arguments are the expected ones
-    expectEvent(receipt, 'Transfer', {
-      from: sender,
-      to: receiver,
-      value: value,
-    });
+    await expect(
+      contract1.transfer(receiverAddress, value, {
+        from: senderAddress,
+      })
+    )
+      .to.emit(contract1, 'Transfer')
+      .withArgs(senderAddress, receiverAddress, value);
   });
 });
