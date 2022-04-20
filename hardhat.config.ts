d5dcf2b03a88fef0c0cd3ca82dc65e76baa83e27
Emre Tepe <emretepedev@gmail.com>
2022-04-20 07:14:30 +0300

    added new networks to deploy and verify the contracts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -22,36 +22,25 @@ task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
   }
 });
 
+function getWallet(): Array<string> {
+  return process.env.DEPLOYER_WALLET_PRIVATE_KEY !== undefined
+    ? [process.env.DEPLOYER_WALLET_PRIVATE_KEY]
+    : [];
+}
+
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
 const config: HardhatUserConfig = {
+  solidity: process.env.SOLIDITY_VERSION || '0.8.7',
   abiExporter: {
-    path: 'soldata/abi/',
+    path: './soldata/abi',
     runOnCompile: true,
     clear: true,
     flat: true,
   },
-  solidity: process.env.SOLIDITY_VERSION || '0.8.7',
-  networks: {
-    ropsten: {
-      url: process.env.ROPSTEN_URL || '',
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    },
-    rinkeby: {
-      url: process.env.RINKEBY_URL || '',
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    },
-    bscTestnet: {
-      url: process.env.BSC_TESTNET_URL || '',
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    },
-  },
   docgen: {
-    path: 'docs/',
+    path: './docs',
     clear: true,
     runOnCompile: true,
   },
@@ -63,15 +52,84 @@ const config: HardhatUserConfig = {
     coinmarketcap: process.env.COINMARKETCAP_API_KEY || '',
     currency: 'USD',
   },
-  etherscan: {
-    apiKey: {
-      ropsten: process.env.ETHERSCAN_API_KEY || '',
-      rinkeby: process.env.ETHERSCAN_API_KEY || '',
-      bscTestnet: process.env.BSCSCAN_API_KEY || '',
-    },
-  },
   spdxLicenseIdentifier: {
     runOnCompile: true,
+  },
+  networks: {
+    arbitrumTestnet: {
+      url: process.env.ARBITRUM_TESTNET_RPC_URL || '',
+      accounts: getWallet(),
+    },
+    auroraTestnet: {
+      url: process.env.AURORA_TESTNET_RPC_URL || '',
+      accounts: getWallet(),
+    },
+    avalancheFujiTestnet: {
+      url: process.env.AVALANCHE_FUJI_TESTNET_RPC_URL || '',
+      accounts: getWallet(),
+    },
+    bscTestnet: {
+      url: process.env.BSC_TESTNET_RPC_URL || '',
+      accounts: getWallet(),
+    },
+    ftmTestnet: {
+      url: process.env.FTM_TESTNET_RPC_URL || '',
+      accounts: getWallet(),
+    },
+    goerli: {
+      url: process.env.GOERLI_RPC_URL || '',
+      accounts: getWallet(),
+    },
+    harmonyTest: {
+      url: process.env.HARMONY_TEST_RPC_URL || '',
+      accounts: getWallet(),
+    },
+    hecoTestnet: {
+      url: process.env.HECO_TESTNET_RPC_URL || '',
+      accounts: getWallet(),
+    },
+    kovan: {
+      url: process.env.KOVAN_RPC_URL || '',
+      accounts: getWallet(),
+    },
+    moonbaseAlpha: {
+      url: process.env.MOONBASE_ALPHA_RPC_URL || '',
+      accounts: getWallet(),
+    },
+    polygonMumbai: {
+      url: process.env.POLYGON_MUMBAI_RPC_URL || '',
+      accounts: getWallet(),
+    },
+    rinkeby: {
+      url: process.env.RINKEBY_RPC_URL || '',
+      accounts: getWallet(),
+    },
+    ropsten: {
+      url: process.env.ROPSTEN_RPC_URL || '',
+      accounts: getWallet(),
+    },
+    sokol: {
+      url: process.env.SOKOL_RPC_URL || '',
+      accounts: getWallet(),
+    },
+  },
+  etherscan: {
+    apiKey: {
+      arbitrumTestnet: process.env.ARBISCAN_API_KEY || '',
+      auroraTestnet: process.env.AURORA_API_KEY || '',
+      avalancheFujiTestnet: process.env.SNOWTRACE_API_KEY || '',
+      bscTestnet: process.env.BSCSCAN_API_KEY || '',
+      ftmTestnet: process.env.FTMSCAN_API_KEY || '',
+      harmonyTest: process.env.HARMONY_POPS_API_KEY || '',
+      hecoTestnet: process.env.HECOINFO_API_KEY || '',
+      goerli: process.env.ETHERSCAN_API_KEY || '',
+      kovan: process.env.ETHERSCAN_API_KEY || '',
+      moonbaseAlpha: process.env.MOONSCAN_API_KEY || '',
+      polygonMumbai: process.env.POLYGONSCAN_API_KEY || '',
+      rinkeby: process.env.ETHERSCAN_API_KEY || '',
+      ropsten: process.env.ETHERSCAN_API_KEY || '',
+      sokol: process.env.BLOCKSCOUT_API_KEY || '',
+    },
   },
 };
 
