0f991f683bfdaa1bc9d4009344265d9d92f81815
emretepedev <emretepedev@gmail.com>
2022-04-22 09:12:50 +0000

    updated tests

--- test/Workshop.test.ts
+++ test/Workshop.test.ts
@@ -1,16 +1,17 @@
 import { expect, assert } from 'chai';
-import { Contract, ContractFactory, Signer, constants } from 'ethers';
+import { Contract, ContractFactory, constants } from 'ethers';
 import { ethers } from 'hardhat';
 
-const contractName: string = 'FooToken';
+const workshop: string = 'Workshop';
+const workshopConstructorArgs: Array<string | number> = ['100000000000000'];
 
-describe(contractName, () => {
-  let contract: Contract;
-  let signer: Signer, signer1: Signer;
+describe(workshop, () => {
+  let workshopContract: Contract;
   let address: string, address1: string;
 
   before(async () => {
-    [signer, signer1] = await ethers.getSigners();
+    const [signer, signer1] = await ethers.getSigners();
+
     [address, address1] = [
       await signer.getAddress(),
       await signer1.getAddress(),
@@ -18,33 +19,34 @@ describe(contractName, () => {
   });
 
   beforeEach(async () => {
-    const Contract: ContractFactory = await ethers.getContractFactory(
-      contractName
+    const workshopContractFactory: ContractFactory =
+      await ethers.getContractFactory(workshop);
+    workshopContract = await workshopContractFactory.deploy(
+      ...workshopConstructorArgs
     );
-    contract = await Contract.deploy('100000000000000');
-    await contract.deployed();
+    await workshopContract.deployed();
   });
 
   it('the token name should be correct', async () => {
-    const name: string = await contract.name();
+    const name: string = await workshopContract.name();
 
     assert.equal(name, 'Foo Token', 'The token name must be valid.');
   });
 
   it('the token symbol should be correct', async () => {
-    const symbol: string = await contract.symbol();
+    const symbol: string = await workshopContract.symbol();
 
     assert.equal(symbol, 'FOO', 'The token symbol must be valid.');
   });
 
   it('the token decimal should be correct', async () => {
-    const decimals: string = await contract.decimals();
+    const decimals: string = await workshopContract.decimals();
 
     assert.equal(decimals, '8', 'The token decimal must be valid.');
   });
 
   it('the token supply should be correct', async () => {
-    const supply: string = await contract.totalSupply();
+    const supply: string = await workshopContract.totalSupply();
 
     assert.equal(supply, '100000000000000', 'The token supply must be valid.');
   });
@@ -52,7 +54,7 @@ describe(contractName, () => {
   it('reverts when transferring tokens to the zero address', async () => {
     // Conditions that trigger a require statement can be precisely tested
     await expect(
-      contract.transfer(constants.AddressZero, constants.One, {
+      workshopContract.transfer(constants.AddressZero, constants.One, {
         from: address,
       })
     ).to.be.revertedWith('ERC20: transfer to the zero address');
@@ -60,11 +62,11 @@ describe(contractName, () => {
 
   it('emits a Transfer event on successful transfers', async () => {
     await expect(
-      contract.transfer(address1, constants.One, {
+      workshopContract.transfer(address1, constants.One, {
         from: address,
       })
     )
-      .to.emit(contract, 'Transfer')
+      .to.emit(workshopContract, 'Transfer')
       .withArgs(address, address1, constants.One);
   });
 });
