743b9c5f3ddaec877cf5b9ee2ad4ce0ccb190ed1
emretepedev <emretepedev@gmail.com>
2022-05-09 00:27:49 +0300

    added array type to constructor args

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -16,14 +16,18 @@ async function main() {
 
   // We get the contract to deploy
   const c1Name: string = 'FooToken';
-  const c1ConstructorArgs: Array<string | number> = ['100000000000000'];
+  const c1ConstructorArgs: Array<string | number | Array<string | number>> = [
+    '100000000000000',
+  ];
   const c1Factory: ContractFactory = await ethers.getContractFactory(c1Name);
   const c1: Contract = await c1Factory.deploy(...c1ConstructorArgs);
   await c1.deployed();
   console.log(c1Name + ' deployed to:', c1.address);
 
   const c2Name: string = 'Workshop';
-  const c2ConstructorArgs: Array<string | number> = [c1.address];
+  const c2ConstructorArgs: Array<string | number | Array<string | number>> = [
+    c1.address,
+  ];
   const c2Factory: ContractFactory = await ethers.getContractFactory(c2Name);
   const c2: Contract = await c2Factory.deploy(...c2ConstructorArgs);
   await c2.deployed();
