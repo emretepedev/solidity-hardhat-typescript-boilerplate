f4f4eaa08a33140960b11d73378f82e4dc358e03
emretepedev <emretepedev@gmail.com>
2024-12-29 15:06:12 +0300

    chore: update Solidity version to ^0.8.28

--- contracts/Timers.sol
+++ contracts/Timers.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 // solhint-disable-next-line compiler-version
-pragma solidity ^0.8.26;
+pragma solidity ^0.8.28;
 
 /**
  * @dev Tooling for timepoints, timers and delays
