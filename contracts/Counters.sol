a754b6b4c26175ddb5576503768267fc3acf44d8
emretepedev <emretepedev@gmail.com>
2024-12-29 14:14:17 +0300

    refactor: Counters library to improve counter conversion functions

--- contracts/Counters.sol
+++ contracts/Counters.sol
@@ -6,18 +6,26 @@ library Counters {
     type Counter is uint256;
 
     function current(Counter counter) internal pure returns (uint256) {
-        return Counter.unwrap(counter);
+        return toUint256(counter);
     }
 
     function increment(Counter counter) internal pure returns (Counter) {
-        return Counter.wrap(Counter.unwrap(counter) + 1);
+        return toCounter(toUint256(counter) + 1);
     }
 
     function decrement(Counter counter) internal pure returns (Counter) {
-        uint256 value = Counter.unwrap(counter);
+        uint256 value = toUint256(counter);
         // solhint-disable-next-line gas-custom-errors
         require(value > 0, "Counter: decrement overflow");
 
-        return Counter.wrap(value - 1);
+        return toCounter(value - 1);
+    }
+
+    function toUint256(Counter counter) internal pure returns (uint256) {
+        return Counter.unwrap(counter);
+    }
+
+    function toCounter(uint256 counter) internal pure returns (Counter) {
+        return Counter.wrap(counter);
     }
 }
